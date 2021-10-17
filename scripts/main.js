e20fc2d6c87c2937a6aee3e6a99e33d2efecc491
Ih8Coding4fun <ashabbi00@gmail.com>
2021-10-17 11:48:48 -0400

    started the html page scaffolding

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+export function saveData() {
+    // capture the user input data
+    var userInput = document.getElementById("userTextInput").value;
+    alert('Data entered: ' + userInput);
+
+    // Save the data
+    var fs = require('fs');  // allows this script to work with the computer's file system.
+    fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+}
+
